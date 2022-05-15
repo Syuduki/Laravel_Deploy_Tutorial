5c1e85e466fadb776472495889e29447d1620166
Syuduki <yudumashi@gmail.com>
2022-05-15 21:51:40 +0900

    add : Todomigration追加

---
+++ sample-api/database/seeders/TodoSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class TodoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i=1; $i < 8; $i++){
+            DB::table('todo')->insert([
+                'id' => $i,
+                'title' => Str::random(10).'の作業',
+                'contents' => Str::random(10).'の内容',
+                'dead_line' => '2022/02/0'.$i,
+                'complate' => false,
+            ]);
+        }
+        
+    }
+}
